9cd77b669742c9d34aa9b6be40327f064274b87f
return_nihil <ardan.exp@gmail.com>
2025-05-21 13:47:48 +0200

    classifiers, removers

--- models/classifiers.py
+++ models/classifiers.py
@@ -14,3 +14,19 @@ class Classifier(nn.Module):
 
     def forward(self, z):
         return self.net(z)
+    
+
+class Remover(nn.Module):
+    '''Remove module for adversarial disentanglement of the latent space'''
+    def __init__(self, input_dim, n_classes):
+        super(Remover, self).__init__()
+        self.ney = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 64),
+            nn.LeakyReLU(),
+            nn.Linear(64, n_classes)
+        )
+
+    def forward(self, x):
+        return self.net(x)
